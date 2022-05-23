02fa5452fcfa29ce75e49c6125bc13aa650c438f
Jaerazone <seren0120@naver.com>
2022-05-23 09:09:40 +0900

    apple

--- STUDY/apple/REACT CLASS/blog/src/App.js
+++ STUDY/apple/REACT CLASS/blog/src/App.js
@@ -5,50 +5,49 @@ import './App.css';
 function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '코딩 학원 추천']);
-  let posts = '강남 고기 맛집';
-
   let [따봉, 따봉변경] = useState(0); //[state, state 변경함수] 이런식으로 적어야 재랜더링이 일어남
-
   let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
+
+  let [입력값, 입력값변경] = useState([]);
   
-  // function 제목바꾸기() {
-  //   let newArray = [...글제목];
-  //   newArray[0] = '여자 코트 추천';
-  //   글제목변경( newArray );
-  // }
-      //
-  // function 순서바꾸기() {
-  //   let newArray = [...글제목];
-  //   newArray.sort();
-  //   글제목변경( newArray );
-  // }
+
+  const addMemo = () => {
+
+    const 새로운글제목 = 글제목.concat([입력값])
+    글제목변경(새로운글제목)
+    console.log(새로운글제목)
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      {/* <button onClick={ 제목바꾸기 }>제목바꾸기버튼</button> */}
-      {/* <button onClick={ 순서바꾸기 }>순서바꾸기버튼</button> */}
-
-
       {
-        글제목.map(function(글){
+        글제목.map(function(글, i){
           return (
-            <div className="list">
-            <h4> { 글 }  <span onClick={ ()=>{ 따봉변경(따봉+1) } } >👍</span> {따봉}   </h4>
+            <div className="list" key={i} >
+            <h4 onClick={ () => {누른제목변경(i)}} > { 글 }  <span onClick={ ()=>{ 따봉변경(따봉+1) } } >👍</span> {따봉}   </h4>
               <p>2월 17일 발행</p>
               <hr /> 
             </div>
           )
         })
       }
-      
+
+      <div className='publish'>
+        {입력값}
+        <input type="text"  onChange={ (e) =>{입력값변경(e.target.value)} } />
+        <button onClick={ ()=>{addMemo()} }>전송</button>
+      </div>
+
+
 
       <button onClick={ ()=>{ modal변경(!modal) } }> 열고닫는버튼 </button>
         {
             modal === true
-            ? <Modal />
+            ? <Modal 글제목={글제목} 누른제목={누른제목} />
             : null
         }
         
@@ -56,10 +55,10 @@ function App() {
   );
 }
 
-function Modal(){
+function Modal(props){
   return (
     <div className="modal">
-      <h2>제목 </h2>
+      <h2>제목 {props.글제목[props.누른제목]} </h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
