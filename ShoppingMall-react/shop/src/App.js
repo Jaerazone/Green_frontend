94359e758699fc9ba03ade3796cb472c8dbca030
Jaerazone <seren0120@naver.com>
2022-06-05 19:03:56 +0900

    shop useEffect setTimeout

--- ShoppingMall-react/shop/src/App.js
+++ ShoppingMall-react/shop/src/App.js
@@ -7,137 +7,122 @@ import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import DetailPage from "./components/DetailPage";
 
 function App() {
-    const [shoes, setShoes] = useState(Data);
-    const navigate = useNavigate();
+  const [shoes, setShoes] = useState(Data);
+  const navigate = useNavigate();
 
-    return (
-        <div className="App">
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">Shoe-Shop</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link
-                                onClick={() => {
-                                    navigate("/");
-                                }}
-                            >
-                                Home
-                            </Nav.Link>
-                            <Nav.Link
-                                onClick={() => {
-                                    navigate("/detail");
-                                }}
-                            >
-                                Cart
-                            </Nav.Link>
-                            <NavDropdown
-                                title="Dropdown"
-                                id="basic-nav-dropdown"
-                            >
-                                <NavDropdown.Item href="#action/3.1">
-                                    Action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">
-                                    Something
-                                </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Shoe-Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                onClick={() => {
+                  navigate("/detail");
+                }}
+              >
+                Cart
+              </Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                ``
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <>
-                            <div className="jumbo__container">
-                                <h1> 20% Season Off </h1>
-                                <p>
-                                    I have not failed.
-                                    I've just found 10,000 ways that won't work.
-                                    -Thomas A. Edison-
-                                    나는 실패하지 않았다. 작동하지 않는 10,000가지 방법을 찾았습니다.
-                                    -토마스 A. 에디슨-
-                                </p>
-                                <p>
-                                    <Button variant="primary">
-                                        Learn more
-                                    </Button>
-                                </p>
-                            </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="jumbo__container">
+                <h1> 20% Season Off </h1>
+                <p>
+                  I have not failed.
+                  I've just found 10,000 ways that won't work.
+                  -Thomas A. Edison-
+                  나는 실패하지 않았다. 작동하지 않는 10,000가지 방법을 찾았습니다.
+                  -토마스 A. 에디슨-
+                </p>
+                <p>
+                  <Button variant="primary">Learn more</Button>
+                </p>
+              </div>
 
-                            <div className="container">
-                                <div className="row">
-                                    {shoes.map((ssss, i) => (
-                                        <Card key={i} sh={shoes[i]} />
-                                    ))}
-                                </div>
-                            </div>
-                        </>
-                    }
-                />
-                <Route
-                    path="/detail/:id"
-                    element={<DetailPage shoes={shoes} />}
-                />
-                <Route path="*" element={<>없는페이지에요</>} />
+              <div className="container">
+                <div className="row">
+                  {shoes.map((ssss, i) => (
+                    <Card key={i} sh={shoes[i]} />
+                  ))}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail/:id" element={<DetailPage shoes={shoes} />} />
+        <Route path="*" element={<>없는페이지에요</>} />
 
-                <Route path="/about" element={<About />}>
-                    <Route path="member" element={<div>멤버임</div>} />
-                    <Route path="location" element={<div>주소임</div>} />
-                </Route>
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<div>주소임</div>} />
+        </Route>
 
-                <Route path="/event" element={<Event />}>
-                    <Route
-                        path="one"
-                        element={<p>첫 주문시 양배추즙 서비스</p>}
-                    />
-                    <Route path="two" element={<p>생일기념 쿠폰받기</p>} />
-                </Route>
-            </Routes>
-        </div>
-    );
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>} />
+          <Route path="two" element={<p>생일기념 쿠폰받기</p>} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 function Event() {
-    return (
-        <div>
-            <h4>오늘의 이벤트</h4>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  );
 }
 
 function About() {
-    return (
-        <div>
-            <h4>회사정보임</h4>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  );
 }
 
 function Card(props) {
-    return (
-        <div className="col-md-4">
-            <img
-                src={require(`./img/food${props.sh.id + 1}.jpg`)}
-                width="100%"
-            />
-            <h4>{props.sh.title}</h4>
-            <p>{props.sh.content} </p>
-            <p>{props.sh.price}</p>
-        </div>
-    );
+  return (
+    <div className="col-md-4">
+      <img src={require(`./img/food${props.sh.id + 1}.jpg`)} width="100%" />
+      <h4>{props.sh.title}</h4>
+      <p>{props.sh.content} </p>
+      <p>{props.sh.price}</p>
+    </div>
+  );
 }
 
 export default App;
--- ShoppingMall-react/shop/src/components/DetailPage.jsx
+++ ShoppingMall-react/shop/src/components/DetailPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -15,7 +15,30 @@ const YellowBtn = styled.button`
 
 const NewBtn = styled.button(YellowBtn); // 기존 스타일 복사가능
 
+const MyAlert = styled.div`
+  background-color: #eeeeee;
+  padding: 15px;
+  border-radius: 5px;
+  max-width: 500px;
+  width: 100%;
+  margin: auto;
+`;
+
+const MyAlert2 = styled(MyAlert)`
+  color: green;
+`;
+
 const DetailPage = (props) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+  }, []);
+
+  const [alert, setAlert] = useState(true);
+  const [count, setCount] = useState(0);
+  const [inputtext, setInputtext] = useState(0);
+
   const { id } = useParams();
   console.log(id);
   const shoesItem = props.shoes.find((item) => item.id == id);
@@ -24,23 +47,53 @@ const DetailPage = (props) => {
   return (
     <div>
       <div className="container">
+        {alert == true ? (
+          <div className="alert alert-warning">2초이내 구매시 할인</div>
+        ) : null}
+        {count}
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          버튼
+        </button>
         <Box>
           <YellowBtn bg="blue">버튼</YellowBtn>
           <YellowBtn bg="yellow">버튼</YellowBtn>
           <YellowBtn bg="rgb(253, 186, 242)">버튼</YellowBtn>
         </Box>
+        <MyAlert2>
+          <p>재고가 얼마 남지 않았습니다</p>
+        </MyAlert2>
         <div className="row">
           <div className="col-md-6">
             <img
               src={require(`../img/food${shoesItem.id + 1}.jpg`)}
               alt=""
-              width="100%"
+              width="50%"
             />
           </div>
           <div className="col-md-6">
             <h4 className="pt-5">{shoesItem.title}</h4>
             <p>{shoesItem.content}</p>
-            <p>{shoesItem.price}원</p>
+            <p>
+              {shoesItem.price}원
+              {isNaN(inputtext) == true ? (
+                <p>숫자만 입력해주세요</p>
+              ) : (
+                `* ${inputtext} 개 = 총 ${shoesItem.price * inputtext}원`
+              )}
+            </p>
+            <br />
+            <input
+              type="text"
+              placeholder="수량을 입력하세요"
+              onChange={(e) => {
+                setInputtext(e.target.value);
+              }}
+            />
+            <br />
             <button className="btn btn-danger">주문하기</button>
             <YellowBtn bg="blue"></YellowBtn>
             <YellowBtn bg={"red"}></YellowBtn>
