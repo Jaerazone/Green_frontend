693f1c47a1f66e76b4334245931e05f248736aeb
Jaerazone <seren0120@naver.com>
2022-06-09 22:45:58 +0900

    redux payload

--- ShoppingMall-react/shop/src/page/Cart.jsx
+++ ShoppingMall-react/shop/src/page/Cart.jsx
@@ -1,43 +1,84 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Table } from "react-bootstrap";
+import { FaWindowClose } from "react-icons/fa";
+import "./Cart.css";
+import { stockAdd, changeName, addAge } from "./../store";
 
 const Cart = () => {
-    return (
-        <div>
-            <Table striped bordered hover size="sm">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>상품명</th>
-                        <th>수량</th>
-                        <th>변경하기</th>
-                        <th>??전체?</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>안녕</td>
-                        <td>안녕</td>
-                        <td>안녕</td>
-                        <td rowSpan={3}>안녕</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>하이</td>
-                        <td>하이</td>
-                        <td>하이</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>3</td>
-                        <td>3</td>
-                        <td>3</td>
-                    </tr>
-                </tbody>
-            </Table>
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  const cart = useSelector((state) => state.cart);
+
+  return (
+    <div>
+      <div className="check_all">
+        <div className="check_inner">
+          <div className="check_box">
+            <button role="checkbox" className="btn_check" aria-checked="false">
+              <span className="check">
+                <FaWindowClose /> 전체 선택
+              </span>
+            </button>
+          </div>
+          <button className="btn_delete">
+            아이콘
+            <span>선택 삭제</span>
+          </button>
         </div>
-    );
+      </div>
+      <div className="">
+        <h6>
+          {state.user.age}살의 {state.user.name}의 장바구니
+        </h6>
+        <button
+          onClick={() => {
+            dispatch(addAge());
+          }}
+        >
+          나이한살더추가
+        </button>
+        <button
+          onClick={() => {
+            dispatch(changeName());
+          }}
+        >
+          이름변경
+        </button>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>사용자이름</th>
+              <th>상품명</th>
+              <th>수량</th>
+              <th>개당 금액</th>
+              <th>??전체?</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((cart, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{cart.name}</td>
+                <td>{cart.stock}</td>
+                <td>{cart.price}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      dispatch(stockAdd(index));
+                    }}
+                  >
+                    +
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- ShoppingMall-react/shop/src/store.js
+++ ShoppingMall-react/shop/src/store.js
@@ -1,10 +1,46 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-createSlice({
-    name: "state이름",
-    initialState: "값",
+const user = createSlice({
+  name: "user",
+  initialState: { name: "kim", age: 20 }, //object 자료형
+  reducers: {
+    changeName(state) {
+      state.name = "park";
+    },
+    addAge(state) {
+      state.age += 1;
+    },
+  },
 });
 
+export const { changeName, addAge } = user.actions;
+
+// const stock = createSlice({
+//   name: "stock",
+//   initialState: [10, 11, 12],
+// });
+
+const cart = createSlice({
+  name: "cart",
+  initialState: [
+    { id: 0, name: "닭가슴살&채소 오리지널 50g", price: 12000, stock: 2 },
+    { id: 2, name: "소고기&채소 오리지널 100g", price: 15000, stock: 1 },
+  ],
+  reducers: {
+    stockAdd(state, a) {
+      //console.log(a.payload);
+      //console.log(state[1].id);
+      state[a.payload].stock = state[a.payload].stock + 1;
+      //console.log(state[a.payload].stock);
+    },
+  },
+});
+export const { stockAdd } = cart.actions;
+
 export default configureStore({
-    reducer: {},
+  reducer: {
+    user: user.reducer, // 작명 : user.reducer
+    // stock: stock.reducer,
+    cart: cart.reducer,
+  },
 });
