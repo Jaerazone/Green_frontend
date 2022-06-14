47c333a8fbea0ab678b2af96928089d5e534d139
Jaera <99634875+Jaerazone@users.noreply.github.com>
2022-06-14 09:21:52 +0900

    react redux & hook green study

--- Green-REACT/11_리덕스/redux_study/src/App.js
+++ Green-REACT/11_리덕스/redux_study/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
-import CounterContainer from "./containers/CounterContainer";
-
+import CounterContainer from "./containers/ConterContainer";
+import CounterHookContainer from "./containers/ConterHookContainer";
+import TodosContainer from "./containers/TodosContainer";
+import TodocHookContainer from "./containers/TodosHookContainer";
 function App() {
     return (
         <div className="App">
             <CounterContainer />
+            <CounterHookContainer />
+            <hr></hr>
+            <TodosContainer />
+            <TodocHookContainer />
         </div>
     );
 }
